7ec3c299dbe1ded1dce6f2a8e6701749eca4e6fd
vtrudnikova <violetta8trudnikova@gmail.com>
2021-08-29 15:19:18 +0300

    edit settings

--- src/test/java/ru/paragon/helpers/DriverSettings.java
+++ src/test/java/ru/paragon/helpers/DriverSettings.java
@@ -4,6 +4,7 @@ import ru.paragon.config.Project;
 import com.codeborne.selenide.Configuration;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import ru.paragon.tests.pages.ParagonMainPage;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,6 +15,7 @@ public class DriverSettings {
         Configuration.browser = Project.config.browser();
         Configuration.browserVersion = Project.config.browserVersion();
         Configuration.browserSize = Project.config.browserSize();
+        ParagonMainPage startPage = new ParagonMainPage();
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         ChromeOptions chromeOptions = new ChromeOptions();
@@ -28,12 +30,17 @@ public class DriverSettings {
             Map<String, Object> mobileDevice = new HashMap<>();
             mobileDevice.put("deviceName", Project.config.browserMobileView());
             chromeOptions.setExperimentalOption("mobileEmulation", mobileDevice);
+
+            startPage.openPage();
+            startPage.changesLocalization("Русский");
         }
 
         if (Project.isRemoteWebDriver()) {
             capabilities.setCapability("enableVNC", true);
             capabilities.setCapability("enableVideo", true);
             Configuration.remote = Project.config.remoteDriverUrl();
+            startPage.openPage();
+            startPage.changesLocalization("Русский");
         }
 
         capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
