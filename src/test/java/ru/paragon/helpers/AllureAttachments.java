ec5070b10a89a8a426bddbd260bb114f60b103f4
vtrudnikova <violetta8trudnikova@gmail.com>
2021-10-12 23:10:48 +0300

    edit

--- src/test/java/ru/paragon/helpers/AllureAttachments.java
+++ src/test/java/ru/paragon/helpers/AllureAttachments.java
@@ -1,5 +1,6 @@
 package ru.paragon.helpers;
 
+import com.codeborne.selenide.Selenide;
 import io.qameta.allure.Allure;
 import io.qameta.allure.Attachment;
 import org.slf4j.Logger;
@@ -9,8 +10,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-
-import static com.codeborne.selenide.Selenide.sleep;
 
 public class AllureAttachments {
     public static final Logger LOGGER = LoggerFactory.getLogger(AllureAttachments.class);
@@ -38,14 +37,14 @@ public class AllureAttachments {
         URL videoUrl = DriverUtils.getVideoUrl(sessionId);
         if (videoUrl != null) {
             InputStream videoInputStream = null;
-            sleep(1000);
+            Selenide.sleep(1000);
 
             for (int i = 0; i < 10; i++) {
                 try {
                     videoInputStream = videoUrl.openStream();
                     break;
                 } catch (FileNotFoundException e) {
-                    sleep(1000);
+                    Selenide.sleep(1000);
                 } catch (IOException e) {
                     LOGGER.warn("[ALLURE VIDEO ATTACHMENT ERROR] Cant attach allure video, {}", videoUrl);
                     e.printStackTrace();
