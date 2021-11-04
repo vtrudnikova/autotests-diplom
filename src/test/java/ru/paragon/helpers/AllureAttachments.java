37ce8aebdf969f6c0e6b2e30943ba99d7a3d1473
vtrudnikova <violetta8trudnikova@gmail.com>
2021-11-04 20:18:48 +0300

    fix

--- src/test/java/ru/paragon/helpers/AllureAttachments.java
+++ src/test/java/ru/paragon/helpers/AllureAttachments.java
@@ -50,7 +50,9 @@ public class AllureAttachments {
                     e.printStackTrace();
                 }
             }
-            Allure.addAttachment("Video", "video/mp4", videoInputStream, "mp4");
+
+            if (videoInputStream != null)
+                Allure.addAttachment("Video", "video/mp4", videoInputStream, "mp4");
         }
     }
 
